6a7bca209a697cb87f0f8da67b7cf4b7dcf1a94f
jacques <jmarques1@gmail.com>
2022-01-06 18:06:10 -0300

    first commit

---
+++ node/index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const app = express()
+const port = 3000
+const config = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+};
+
+const mysql = require('mysql');
+const connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Jacques')`;
+connection.query(sql);
+
+app.get('/', (req,res) => {
+    let response = '<h1>Full Cycle Rocks!</h1>';
+    response += '<h2>Pessoas Cadastradas</h2>';
+    connection.query(`SELECT * FROM people`, function (err, result, fields) {
+        // if any error while executing above query, throw error
+        if (err) throw err;
+        
+        // if there is no error, you have the result
+        for(let i=0; i < result.length; i++) {
+            response += result[i].name + '<br>';
+        }
+        res.send(response);
+  
+        connection.end;
+      });
+});
+
+app.listen(port, ()=> {
+    console.log('Rodando na porta: ' + port)
+});
